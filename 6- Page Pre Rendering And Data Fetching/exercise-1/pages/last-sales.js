83ddad31255ad5c18c946ec42eb5d234dd7c5fa9
Hadi90fe <elhadiii90@gmail.com>
2024-02-07 12:03:08 -0500

    :tada: combine client side and server side fetching

--- 6- Page Pre Rendering And Data Fetching/exercise-1/pages/last-sales.js
+++ 6- Page Pre Rendering And Data Fetching/exercise-1/pages/last-sales.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 const fetcher = (url) =>
@@ -15,31 +16,45 @@ const fetcher = (url) =>
             return transformedData;
         });
 
-function LastSalesPage() {
-
+function LastSalesPage(props) {
+    const [sales, setSales] = useState(props.sales);
     const { data, error, isLoading } = useSWR(
         "https://nextjs-course-d483f-default-rtdb.firebaseio.com/sales.json",
         fetcher
     ); // we used fetcher because we needed to transform our data.
 
+    useEffect(() => {
+        // we used useEffect here to combine prefeched data in server side with fetched data in client side
+        setSales(data);
+    }, [data]);
 
     if (error) {
         return <p>Failed to load</p>;
     }
 
-    if (isLoading) {
+    if (!sales || isLoading) {
         return <h1>Loading...</h1>;
     }
 
     return (
         <ul>
-            {data.map((sale) => (
+            {sales.map((sale) => (
                 <li key={sale.id}>
                     {sale.username} - {sale.volume}
                 </li>
             ))}
         </ul>
     );
+}
+
+export async function getStaticProps() {
+    const data = await fetcher(
+        "https://nextjs-course-d483f-default-rtdb.firebaseio.com/sales.json"
+    );
+    return {
+        props: { sales: data },
+        revalidate: 10,
+    };
 }
 
 export default LastSalesPage;
