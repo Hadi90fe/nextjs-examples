a8753cdda95ec7239c4709eac4597ca30124caa0
Hadi90fe <elhadiii90@gmail.com>
2024-02-06 19:39:32 -0500

    :rocket: 2nd module exercices

---
+++ 6- Page Pre Rendering And Data Fetching/exercise-1/pages/[pid].js
@@ -0,0 +1,55 @@
+import path from 'path';
+import fs from 'node:fs/promises';
+
+function ProductDetailPage(props) {
+    const { loadedProduct } = props;
+
+    // if (!loadedProduct) {
+    //     return <p>Loading...</p>;
+    // }
+
+    return <>
+        <h1>{loadedProduct.title}</h1>
+        <p>{loadedProduct.description}</p>
+    </>;
+};
+
+async function getData() {
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData); // convert json data to regular jscript object
+    return data;
+};
+
+export async function getStaticProps(context) {
+    const { params } = context;
+
+    const productId = params.pid;
+
+    const data = await getData();
+    const product = data.products.find((product) => product.id === productId);
+
+    if (!product) {
+        return { notFound: true };
+    }
+
+    return {
+        props: {
+            loadedProduct: product,
+        }
+    };
+};
+
+export async function getStaticPaths() {
+    const data = await getData();
+
+    const ids = data.products.map(product => product.id);
+    const pathsWithParams = ids.map(id => ({ params: { pid: id } }));
+
+    return {
+        paths: pathsWithParams,
+        fallback: "blocking", // or true with => if(!loadedProduct) {return <p>Loading...</p>) in page component
+    };
+}
+
+export default ProductDetailPage;
---
+++ 6- Page Pre Rendering And Data Fetching/exercise-1/pages/index.js
@@ -0,0 +1,39 @@
+import path from 'path';
+import fs from 'node:fs/promises';
+
+import Link from 'next/link';
+
+function HomePage(props) {
+  const { products } = props;
+  return <ul>
+    {products.map((product) => <li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>)}
+  </ul>
+}
+
+export async function getStaticProps(context) {
+  console.log('(Re-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData); // convert json data to regular jscript object
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10
+  };
+}
+
+export default HomePage;
